f7ea88144b53c822e5fb0bb73fa9f9a75dd6de10
ayusha1223 <ayushathapa425@gmail.com>
2025-12-25 20:36:35 +0545

    Added Picture

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import LoginForm from "../components/auth/LoginForm";
 
 
@@ -5,6 +6,16 @@ export default function LoginPage() {
   return (
     <div className="auth-container">
       <div className="auth-card">
+        {/* Logo */}
+        <div style={{ textAlign: "center", marginBottom: "15px" }}>
+          <Image
+            src="/images/login.png"
+            alt="Naayu Attires Logo"
+            width={80}
+            height={80}
+          />
+        </div>
+
         <h1>Login</h1>
         <LoginForm />
       </div>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
 import RegisterForm from "../components/auth/RegisterForm";
-
 
 export default function RegisterPage() {
   return (
-    <>
-      <h1>Register</h1>
-      <RegisterForm />
-    </>
+    <div className="auth-container">
+      <div className="auth-card">
+
+        {/* Logo */}
+        <div style={{ textAlign: "center", marginBottom: "15px" }}>
+          <Image
+            src="/images/login.png"
+            alt="Naayu Attires Logo"
+            width={80}
+            height={80}
+          />
+        </div>
+
+        <h1>Register</h1>
+        <RegisterForm />
+      </div>
+    </div>
   );
 }
