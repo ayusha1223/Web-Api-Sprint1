143ac0f4f9be7c4f3c962da83b271e02807037ec
ayusha1223 <ayushathapa425@gmail.com>
2025-12-25 20:12:22 +0545

    RegisterForm

---
+++ app/components/auth/RegisterForm.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { registerSchema } from "@/schemas/registerSchema";
+import Input from "../ui/Input";
+import Button from "../ui/Button";
+
+export default function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input label="Name" {...register("name")} error={errors.name?.message as string} />
+      <Input label="Email" {...register("email")} error={errors.email?.message as string} />
+      <Input label="Password" type="password" {...register("password")} error={errors.password?.message as string} />
+      <Button text="Register" />
+    </form>
+  );
+}
