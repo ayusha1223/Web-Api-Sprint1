9da4901455c6f5658487e497ca417298c9156f58
ayusha1223 <ayushathapa425@gmail.com>
2025-12-25 20:13:30 +0545

    RegisterPage

---
+++ app/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "../components/auth/RegisterForm";
+
+
+export default function RegisterPage() {
+  return (
+    <>
+      <h1>Register</h1>
+      <RegisterForm />
+    </>
+  );
+}
