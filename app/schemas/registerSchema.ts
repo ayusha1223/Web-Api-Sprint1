1c76a87db99dfd89749445a6f4256e07afc789c0
ayusha1223 <ayushathapa425@gmail.com>
2026-01-20 10:02:10 +0545

    register.schema

--- app/schemas/registerSchema.ts
+++ app/schemas/registerSchema.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-  name: z.string().min(2, "Name is required"),
+  name: z.string().min(2, "Name too short"),
   email: z.string().email("Invalid email"),
-  phone: z.string().min(10, "Invalid phone number"),
-  password: z.string().min(6, "Password must be at least 6 characters"),
+  password: z.string().min(6, "Password too short"),
 });
